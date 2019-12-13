3b6536162393bc1914349ee628b4daa5f6aa6065
Mojo <mj81000@gmail.com>
2019-12-13 15:47:44 -0600

    updated names.py

--- names.py
+++ names.py
@@ -1,51 +1,54 @@
 import random
 
-def getnames(filename):
+
+def get_names(filename):
     try:
         f = open(filename, 'r')
-        _ = f.readline()        #Reads M for Masc names
-        mNames = f.readline().split(', ')
-        _ = f.readline()        #reads F for Femme names
-        fNames = f.readline().split(', ')
-        _ = f.readline()        #reads N for Neutral names
-        nNames = f.readline().split(', ')
-        _ = f.readline()        #reads L for Last names
-        lNames = f.readline().split(', ')
-        return (mNames, fNames, nNames, lNames)
+        _ = f.readline()        # Reads M for Masc names
+        masc_names = f.readline().split(', ')
+        _ = f.readline()        # Reads F for Femme names
+        femme_names = f.readline().split(', ')
+        _ = f.readline()        # Reads N for Neutral names
+        neutral_names = f.readline().split(', ')
+        _ = f.readline()        # Reads L for Last names
+        last_names = f.readline().split(', ')
+        return masc_names, femme_names, neutral_names, last_names
     finally:
         f.close()
 
-def makefullname(first_list, last_list):
+
+def make_full_name(first_list, last_list):
     return "{0} {1}".format(random.choice(first_list), random.choice(last_list))
+
 
 class NameFile:
     """A class for unpacking and using a name file"""
     def __init__(self, filename):
-        self.mNames, self.fNames, self.nNames, self.lNames = getnames(filename)
+        self.masc_names, self.femme_names, self.neutral_names, self.last_names = get_names(filename)
 
     def rand_name(self, gender='a'):
         if gender.capitalize() == 'M':
-            return random.choice(self.mNames)
+            return random.choice(self.masc_names)
         elif gender.capitalize() == 'F':
-            return random.choice(self.fNames)
+            return random.choice(self.femme_names)
         elif gender.capitalize() == 'N':
-            return random.choice(self.nNames)
+            return random.choice(self.neutral_names)
         else:
-            return random.choice(self.mNames+self.fNames+self.nNames)
-
+            return random.choice(self.masc_names + self.femme_names + self.neutral_names)
 
     def rand_full_name(self, gender='a'):
-        if (gender.capitalize() == 'M'):
-            return makefullname(self.mNames, self.lNames)
-        elif (gender.capitalize() == 'F'):
-            return makefullname(self.fNames, self.lNames)
-        elif (gender.capitalize() == 'N'):
-            return makefullname(self.nNames, self.lNames)
+        if gender.capitalize() == 'M':
+            return make_full_name(self.masc_names, self.last_names)
+        elif gender.capitalize() == 'F':
+            return make_full_name(self.femme_names, self.last_names)
+        elif gender.capitalize() == 'N':
+            return make_full_name(self.neutral_names, self.last_names)
         else:
-            return makefullname(self.mNames+self.fNames+self.nNames, self.lNames)
+            return make_full_name(self.masc_names + self.femme_names + self.neutral_names, self.last_names)
+
 
 if __name__ == "__main__":
-    western_names = NameFile("westernnames")
+    western_names = NameFile("westernnames.txt")
     print(western_names.rand_full_name())
     print()
     print(western_names.rand_full_name('m'))
